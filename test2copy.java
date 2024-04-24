d2ca158ad41413c89801b1e606b78f64ee8ec360
Safia666 <69086140+Safia666@users.noreply.github.com>
2024-04-23 23:02:56 -0500

    added duplicate file

---
+++ test2copy.java
@@ -0,0 +1,12 @@
+//Solve the issues with this code
+//Remember to commit correctly after all bugs have been fixed
+//Make sure you have the compiler/interpreter
+
+
+public class test2{ 
+    public static void main(String[] args){
+        
+        System.out.println("Wooohoooo bug solved");
+    
+    }
+}
